def3feb3e55cac9bbede93beabca12007b0f04b5
Ahmad Raza <raza88581@gmail.com>
2021-11-06 14:58:30 +0500

    adding cors options

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const cors = require("cors");
 const fs = require("fs");
 const User = require("./User.Model");
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
