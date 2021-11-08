95974825df9f54b6ca0688520398537b6450fd7a
Ahmad Raza <raza88581@gmail.com>
2021-11-08 11:31:50 +0500

    cors added

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const cors = require("cors");
 const fs = require("fs");
 const User = require("./User.Model");
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
@@ -39,6 +33,30 @@ mongoose
   .catch((err) => {
     console.log(`DB Connection Error: ${err.message}`);
   });
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Working Initial Route!");
